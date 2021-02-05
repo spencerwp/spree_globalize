1a39677ec5fce698a69fba17ec9af7cd0bb59577
spencerwp <sppow93@gmail.com>
2021-02-05 10:38:36 -0500

    Create store_decorator.rb

---
+++ app/models/spree/store_decorator.rb
@@ -0,0 +1,9 @@
+module Spree::StoreDecorator	
+  def self.prepended(base)	
+    base.translates :name, :meta_description, :meta_keywords, :seo_title, fallbacks_for_empty_translations: true	
+  end	
+
+  Spree::Store.include SpreeGlobalize::Translatable	
+end	
+
+::Spree::Store.prepend(Spree::StoreDecorator)
